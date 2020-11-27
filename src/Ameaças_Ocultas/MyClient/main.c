c83107136733b215745b77a502f590ea5179362d
PedroPeixinho <phagp@cin.ufpe.br>
2020-11-27 01:53:38 -0300

    Ajeitar o tom do verde

--- src/Ameaças_Ocultas/MyClient/main.c
+++ src/Ameaças_Ocultas/MyClient/main.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[]) {
     bool redraw = true;
 
     // Display a Green screen
-    al_clear_to_color(al_map_rgb(0, 255, 0));
+    al_clear_to_color(al_map_rgb(0, 130, 0));
     al_flip_display();
 
     // Game loop
@@ -37,7 +37,7 @@ int main(int argc, char *argv[]) {
         // Check if we need to redraw
         if (redraw && al_is_event_queue_empty(event_queue)) {
             // Redraw
-            al_clear_to_color(al_map_rgb(0, 255, 0));
+            al_clear_to_color(al_map_rgb(0, 130, 0));
             al_draw_bitmap(imagem, 256, 256, 0);
             al_flip_display();
             redraw = false;
