cc8eacfa94eec4ae077fa13d3aacad7715d3e4a9
Mauro-Jorge <mjss@cin.ufpe.br>
2020-11-28 22:15:14 -0300

    Identação

---
+++ src/lib/client.h
@@ -0,0 +1,13 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include "lib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+
+void DesenharGrama();
+void DesenharParede();
+
+#endif
